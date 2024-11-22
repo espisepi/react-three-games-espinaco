652fb888e5c61a0e3e08a19b16904c3755c19b82
espisepi <sepinaco@gmail.com>
2024-11-22 18:51:01 +0100

    AppR3F hello world

---
+++ src/features/r3f/AppR3F.tsx
@@ -0,0 +1,11 @@
+const AppR3F = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>HOLA MUNDO AppR3F</h1>
+      </header>
+    </div>
+  )
+}
+
+export default AppR3F
